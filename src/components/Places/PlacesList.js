02a16c33df17f9df3591ef7bee319a5ab4a419c7
Maryam Ebrahim <mxebrahim@gmail.com>
2019-05-05 17:08:19 +0100

    added card layout to placeslist.js

--- src/components/Places/PlacesList.js
+++ src/components/Places/PlacesList.js
@@ -34,50 +34,59 @@ class PlacesList extends Component {
     }
 
     showingPlaces.sort(sortBy('name'));
-    return (
-        <div>
-            <div className="row">
-                <form class="form-inline mt-2 mt-md-0">
-                    {/* {JSON.stringify(this.state)} to view the searched query on UI*/}
-                    <input
-                        className="search form-control"
-                        type="text"
-                        placeholder="search places"
-                        value={this.state.query}
-                        onChange={event => this.updateQuery(event.target.value)}
-                    />
 
-    {/* PlacesList.js-To-DO-LINE-50*** connect the search button below to search bar */}
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    {showingPlaces.length !== places.length && (
-                        <div className="showing-places">
-                            <span>
-                                Now Showing {showingPlaces.length} of {places.length} total
-                            </span>
-                            <button onClick={this.clearQuery}>Show All</button>
-                        </div>
-                    )}
+    return (
+       
+            <div className="row vertical-align">
+                <div className="col-xs-8">
+                <form class="form-inline mt-2 mt-md-0">
+                    
+                        {/* {JSON.stringify(this.state)} to view the searched query on UI*/}
+                        <input
+                            className="search form-control"
+                            type="text"
+                            placeholder="search places"
+                            value={this.state.query}
+                            onChange={event => this.updateQuery(event.target.value)}
+                        />
+
+                        {/* PlacesList.js-To-DO-LINE-50*** connect the search button below to search bar */}
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                            Search
+                        </button>
+                    
+                    
+                        {showingPlaces.length !== places.length && (
+                            <div className="showing-places">
+                                <span>
+                                    Now Showing {showingPlaces.length} of {places.length} total
+                                </span>
+                                <button className="btn btn-outline-secondary my-2 my-sm-0" onClick={this.clearQuery}>Show All</button>
+                            </div>
+                        )}
+                    
                 </form>
-            </div>
+                </div>
+            
             <ol className="place-list">
                 {showingPlaces.map(place => (
                     <li key={place.id} className="place-list-item">
-                        <div
-                            className="place-avatar"
-                            style={{
-                                    backgroundImage: `url(${place.url})`
-                                }}
-                        />
-                        <div className="place-details">
-                            <p> {place.name}</p>
-                            <p>{place.vicinity}</p>
-                            <p>{place.price_leve}</p>
-                            <p>{place.rating}</p>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{place.name}</h5>
+                            <p class="card-text">{place.vicinity}</p>
+                            <p class="card-text"><small class="text-muted">{place.price_leve}</small></p>
+                            <p class="card-text"><small class="text-muted">{place.rating}</small></p>
+                            <p class="card-text"><small class="text-muted">{place.user_ratings_total}</small></p>
+                            
+                        </div>
+                        {/*<img class="card-img-bottom" src="..." alt="Card image cap">*/}
                         </div>
                     </li>
                 ))}
             </ol>
-        </div>
+    </div>
+                
     );
   }
 }
